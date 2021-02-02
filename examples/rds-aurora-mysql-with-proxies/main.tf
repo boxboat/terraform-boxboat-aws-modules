6570b8379ce217a6ef4b0940ef3fccdd8c24f748
Facundo Gauna <fgauna12@gmail.com>
2021-02-02 12:05:01 -0800

    Enabling ingress rule again

--- examples/rds-aurora-mysql-with-proxies/main.tf
+++ examples/rds-aurora-mysql-with-proxies/main.tf
@@ -66,13 +66,13 @@ resource "aws_security_group" "allow_mysql" {
   description = "Allow MySQL inbound traffic"
   vpc_id      = aws_vpc.vpc.id
 
-  # ingress {
-  #   description = "Allow MySQL"
-  #   from_port   = 0
-  #   to_port     = 3306
-  #   protocol    = "tcp"
-  #   cidr_blocks = [aws_vpc.vpc.cidr_block]
-  # }
+  ingress {
+    description = "Allow MySQL"
+    from_port   = 0
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
 
   tags = {
     Name = "allow_mysql"
